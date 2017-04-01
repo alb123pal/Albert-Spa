953c05248eb98f27ae3d53e427bef7d1e8fa78d5
alb123pal <albertpalka1995@gmail.com>
2017-04-01 16:46:53 +0200

    zadanka

---
+++ spa-Albert/03-JavaScriptBasics/js/data.js
@@ -0,0 +1,7 @@
+/**
+ * Created by albert on 20.03.17.
+ */
+
+function pokazDate(){
+    alert(Date());
+}
---
+++ spa-Albert/03-JavaScriptBasics/js/eventScript.js
@@ -0,0 +1,9 @@
+/**
+ * Created by albert on 20.03.17.
+ */
+
+var number = 0;
+function klikKlik() {
+    number++;
+    alert("You clicked on the button " + number );
+}
