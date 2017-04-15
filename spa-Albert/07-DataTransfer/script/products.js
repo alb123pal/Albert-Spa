3a63d29a66455b360523cdd143f22260b998d825
alb123pal <albertpalka1995@gmail.com>
2017-04-15 15:36:05 +0200

    send data to server

--- spa-Albert/07-DataTransfer/script/products.js
+++ spa-Albert/07-DataTransfer/script/products.js
@@ -4,17 +4,35 @@
 
 
 
+// $(document).ready(function() {
+//
+//     $("#przycisk").click(function(event){
+//         $.get(
+//             "result.php",
+//             { name: "Zara" },
+//             function(data) {
+//                 $('#info').html(data);
+//             }
+//         );
+//     });
+//
+// });
+
 $(document).ready(function() {
+    $("#przycisk").click(function(){
+        var numer = ($("#number").val()) - 1;
+        $.getJSON('products.json', function(jd) {
+            console.log("key: " + jd[numer].id);
+             $("#info").empty();
+             $("#info").append("ID: " + jd[numer].id + "<br>");
+             $("#info").append("Name: " + jd[numer].name + "<br>");
+             $("#info").append("Price: " + jd[numer].price + "<br>");
+             $("#info").append("In stock: " + jd[numer].instock + "<br>");
+        });
 
-    $("#przycisk").click(function(event){
-        $.get(
-            "result.php",
-            { name: "Zara" },
-            function(data) {
-                $('#info').html(data);
-            }
-        );
     });
-
 });
 
+// $.getJSON('products.json', function(jd) {
+//         console.log(jd);
+// });
