8995cfe3b3684eb04e4598d124dfed8e61584cb4
alb123pal <albertpalka1995@gmail.com>
2017-05-03 14:54:10 +0200

    table

--- spa-Albert/08-SinglePageApp/view.js
+++ spa-Albert/08-SinglePageApp/view.js
@@ -1,16 +1,72 @@
 var view = {
 		display: function (data) {
 			// create new data view to be displayed
-			var elem, text, i;
-			this.clear();
-			for (i = 0; i < data.length; i++) {
-				elem = document.createElement("h3");
-				text = document.createTextNode(
-						data[i].code + "/" + data[i].name + " " + data[i].rate
-						);
-				elem.appendChild(text);
-				document.getElementById('dataview').appendChild(elem);
-			}
+            this.clear();
+
+            var tbl     = document.createElement("table");
+            var tblHead = document.createElement("thead");
+            var tblBody = document.createElement("tbody");
+
+            var tr = document.createElement("tr");
+            tblHead.appendChild(tr);
+
+            var codeText = document.createTextNode("Code");
+            var nameText = document.createTextNode("Name");
+            var rateText = document.createTextNode("Rate");
+
+            var code = document.createElement("td");
+            code.appendChild(codeText);
+
+            var name = document.createElement("td");
+            name.appendChild(nameText);
+
+            var text = document.createElement("td");
+            text.appendChild(rateText);
+
+            tblHead.appendChild(code);
+            tblHead.appendChild(name);
+            tblHead.appendChild(text);
+
+            document.getElementById('dataview').appendChild(tblHead);
+
+            for (var i = 0; i < data.length; i++){
+                var row = document.createElement("tr");
+                tblBody.appendChild(row);
+
+                var cell1 = document.createElement("td");
+                var cell2 = document.createElement("td");
+                var cell3 = document.createElement("td");
+
+                var cell1Text = document.createTextNode(data[i].code);
+                var cell2Text = document.createTextNode(data[i].name);
+                var cell3Text = document.createTextNode(data[i].rate);
+
+                cell1.appendChild(cell1Text);
+                cell2.appendChild(cell2Text);
+                cell3.appendChild(cell3Text);
+
+                row.appendChild(cell1);
+                row.appendChild(cell2);
+                row.appendChild(cell3);
+
+                tblBody.appendChild(row);
+
+
+            }
+            // append the <tbody> inside the <table>
+            tbl.appendChild(tblHead);
+            tbl.appendChild(tblBody);
+            // put <table> in the <body>
+            document.getElementById('dataview').appendChild(tbl);
+            tbl.setAttribute("border", "4px");
+            tbl.setAttribute("border", "solid");
+
+
+
+
+
+
+
 		},
 		clear: function () {
 			// remove old data
@@ -21,10 +77,12 @@ var view = {
 		},
 		addCurrency: function () {
 			var currency = {
+
 					code: document.getElementById('code').value,
 					rate: document.getElementById('rate').value,
 					name: document.getElementById('name').value
 			}
 			controller.storeCurrency(currency);
+
 		}
 	};
