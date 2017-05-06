89651a11e9397cc32b626ffed552cec0e1221421
alb123pal <albertpalka1995@gmail.com>
2017-05-06 16:38:18 +0200

    local and web database

--- spa-Albert/08-SinglePageApp/model.js
+++ spa-Albert/08-SinglePageApp/model.js
@@ -1,6 +1,23 @@
 var model = {
 		getData: function () {return this.storage; },
-		addData: function (data) {this.storage.push(data); },
+		addData: function (data) {
+			this.storage.push(data);
+			localStorage.clear();
+			localStorage.setItem('waluty', JSON.stringify(this.storage));
+
+			//add to web sql database
+            var db = openDatabase('waluty', '2.0,', 'Test', 3*1024*1024);
+            var msg;
+
+            db.transaction(function (tx) {
+                tx.executeSql('CREATE TABLE IF NOT EXISTS WALUTY (storage)');
+                tx.executeSql('INSERT INTO WALUTY (storage) VALUES (?'), [this.storage];
+                msg = 'Log message created and row inserted.';
+                console.log(msg);
+            });
+
+
+			},
 		delData: function (code) {},
 		storage: [] // array of objects {code, rate, name}
 	};
--- spa-Albert/08-SinglePageApp/view.js
+++ spa-Albert/08-SinglePageApp/view.js
@@ -1,88 +1,84 @@
 var view = {
-		display: function (data) {
-			// create new data view to be displayed
-            this.clear();
+    display: function (data) {
+        // create new data view to be displayed
+        this.clear();
 
-            var tbl     = document.createElement("table");
-            var tblHead = document.createElement("thead");
-            var tblBody = document.createElement("tbody");
+        var tbl = document.createElement("table");
+        var tblHead = document.createElement("thead");
+        var tblBody = document.createElement("tbody");
 
-            var tr = document.createElement("tr");
-            tblHead.appendChild(tr);
+        var tr = document.createElement("tr");
+        tblHead.appendChild(tr);
 
-            var codeText = document.createTextNode("Code");
-            var nameText = document.createTextNode("Name");
-            var rateText = document.createTextNode("Rate");
+        var codeText = document.createTextNode("Code");
+        var nameText = document.createTextNode("Name");
+        var rateText = document.createTextNode("Rate");
 
-            var code = document.createElement("td");
-            code.appendChild(codeText);
+        var code = document.createElement("td");
+        code.appendChild(codeText);
 
-            var name = document.createElement("td");
-            name.appendChild(nameText);
+        var name = document.createElement("td");
+        name.appendChild(nameText);
 
-            var text = document.createElement("td");
-            text.appendChild(rateText);
+        var text = document.createElement("td");
+        text.appendChild(rateText);
 
-            tblHead.appendChild(code);
-            tblHead.appendChild(name);
-            tblHead.appendChild(text);
+        tblHead.appendChild(code);
+        tblHead.appendChild(name);
+        tblHead.appendChild(text);
 
-            document.getElementById('dataview').appendChild(tblHead);
+        document.getElementById('dataview').appendChild(tblHead);
+        console.log(data);
 
-            for (var i = 0; i < data.length; i++){
-                var row = document.createElement("tr");
-                tblBody.appendChild(row);
+        for (var i = 0; i < data.length; i++) {
+            var row = document.createElement("tr");
+            tblBody.appendChild(row);
 
-                var cell1 = document.createElement("td");
-                var cell2 = document.createElement("td");
-                var cell3 = document.createElement("td");
+            var cell1 = document.createElement("td");
+            var cell2 = document.createElement("td");
+            var cell3 = document.createElement("td");
 
-                var cell1Text = document.createTextNode(data[i].code);
-                var cell2Text = document.createTextNode(data[i].name);
-                var cell3Text = document.createTextNode(data[i].rate);
+            var cell1Text = document.createTextNode(data[i].code);
+            var cell2Text = document.createTextNode(data[i].name);
+            var cell3Text = document.createTextNode(data[i].rate);
 
-                cell1.appendChild(cell1Text);
-                cell2.appendChild(cell2Text);
-                cell3.appendChild(cell3Text);
+            cell1.appendChild(cell1Text);
+            cell2.appendChild(cell2Text);
+            cell3.appendChild(cell3Text);
 
-                row.appendChild(cell1);
-                row.appendChild(cell2);
-                row.appendChild(cell3);
+            row.appendChild(cell1);
+            row.appendChild(cell2);
+            row.appendChild(cell3);
 
-                tblBody.appendChild(row);
-
-
-            }
-            // append the <tbody> inside the <table>
-            tbl.appendChild(tblHead);
-            tbl.appendChild(tblBody);
-            // put <table> in the <body>
-            document.getElementById('dataview').appendChild(tbl);
-            tbl.setAttribute("border", "4px");
-            tbl.setAttribute("border", "solid");
+            tblBody.appendChild(row);
 
 
+        }
+        // append the <tbody> inside the <table>
+        tbl.appendChild(tblHead);
+        tbl.appendChild(tblBody);
+        // put <table> in the <body>
+        document.getElementById('dataview').appendChild(tbl);
+        tbl.setAttribute("border", "4px");
+        tbl.setAttribute("border", "solid");
 
 
+    },
+    clear: function () {
+        // remove old data
+        var dataView = document.getElementById('dataview');
+        while (dataView.firstChild) {
+            dataView.removeChild(dataView.firstChild);
+        }
+    },
+    addCurrency: function () {
+        var currency = {
 
+            code: document.getElementById('code').value,
+            rate: document.getElementById('rate').value,
+            name: document.getElementById('name').value
+        };
+        controller.storeCurrency(currency);
 
-
-		},
-		clear: function () {
-			// remove old data
-			var dataView = document.getElementById('dataview');
-			while (dataView.firstChild) {
-				dataView.removeChild(dataView.firstChild);
-			}
-		},
-		addCurrency: function () {
-			var currency = {
-
-					code: document.getElementById('code').value,
-					rate: document.getElementById('rate').value,
-					name: document.getElementById('name').value
-			}
-			controller.storeCurrency(currency);
-
-		}
-	};
+    }
+};
